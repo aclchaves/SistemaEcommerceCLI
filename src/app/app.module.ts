d0f341a3b562430cff5ccab92c830cef65fc62d7
Andre Chaves <aclchaves@gmail.com>
2019-11-17 18:29:02 -0300

    Agora realmente é o primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginServiceService } from './login-service.service';
+import { AuthGuard } from './login-guard';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    LoginServiceService,
+    AuthGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
